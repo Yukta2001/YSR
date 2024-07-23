f1ac72108d7e30baa5b78479ab1336ad13641013
Yukta2001 <70341925+Yukta2001@users.noreply.github.com>
2024-07-23 14:52:46 +0530

    Update user_mst_syncing.py

--- user_mst_syncing.py
+++ user_mst_syncing.py
@@ -5,10 +5,10 @@ import oracledb
 # Oracle connection function
 def get_oracle_connection():
     user_name = 'ARGSRI_FINAL' 
-    password_var = 'ysrStg#423'
-    hostname = '10.10.223.179'
+    password_var = 'ntrvs#123'
+    hostname = '10.9.39.218'
     port = 1521
-    sid = 'sysrasdb'
+    sid = 'pysrasdb'
     
     connection = oracledb.connect(user=user_name, password=password_var,
                                   host=hostname, port=port, sid=sid)
@@ -19,9 +19,9 @@ def get_oracle_connection():
 def create_postgres_connection():
     try:
         # Define connection parameters
-        dbname = "ysr_db"
-        user = "ysr_manager"
-        password = "ysr_user_manager"
+        dbname = "medical_dispensary"
+        user = "ntr_med_disp"
+        password = "ntr_med_disp__@2024"
         host = "172.16.32.168"
         port = "5432"
         
@@ -45,8 +45,24 @@ def create_postgres_connection():
 
 # Oracle DB Query and DataFrame Creation
 cursor = get_oracle_connection()
-query = '''SELECT to_char(first_name||' '||last_name) as user_name,
-au.user_id as emp_code,ah.hosp_id,ah.hosp_name,ah.dist_id hosp_dist_id,
+query1 = '''SELECT to_char(first_name||' '||last_name) as user_name,
+ah.hosp_id,ah.hosp_name,ah.dist_id hosp_dist_id,
+al.loc_name hosp_dist_name,
+aeh.mandal as hosp_mandal_id,
+(SELECT LOC_NAME FROM ASRIM_LOCATIONS WHERE aeh.mandal = loc_id) hospital_mandal,
+CASE WHEN NEW_EMP_CODE IS NOT NULL THEN new_emp_code
+ELSE login_name END as login_id,encripted_password
+FROM asrim_nwh_users anu
+JOIN asrim_users au ON anu.user_id=au.user_id
+JOIN asrim_hospitals ah ON anu.hosp_id = ah.hosp_id
+LEFT JOIN asrim_locations al ON ah.dist_id=al.loc_id
+LEFT join ASRIT_EMPNL_HOSPINFO AEH on AH.HOSP_EMPNL_REF_NUM = AEH.HOSPINFO_ID
+WHERE au.PRIMARY_FLAG = 'Y' AND au.active_yn = 'Y'
+AND anu.eff_end_dt is null
+AND au.crt_dt >= trunc(sysdate) - 1'''
+
+query2 = '''SELECT to_char(first_name||' '||last_name) as user_name,
+ah.hosp_id,ah.hosp_name,ah.dist_id hosp_dist_id,
 al.loc_name hosp_dist_name,
 aeh.mandal as hosp_mandal_id,
 (SELECT LOC_NAME FROM ASRIM_LOCATIONS WHERE aeh.mandal = loc_id) hospital_mandal,
@@ -59,11 +75,22 @@ LEFT JOIN asrim_locations al ON ah.dist_id=al.loc_id
 LEFT join ASRIT_EMPNL_HOSPINFO AEH on AH.HOSP_EMPNL_REF_NUM = AEH.HOSPINFO_ID
 WHERE au.PRIMARY_FLAG = 'Y' 
 AND anu.eff_end_dt is null
-AND (au.crt_dt >= trunc(sysdate) - 1 OR au.lst_upd_dt >= trunc(sysdate) - 1)'''
-cursor.execute(query)
-df = pd.DataFrame(cursor.fetchall(), columns=[col[0] for col in cursor.description])
-print(df)
+AND au.lst_upd_dt >= trunc(sysdate) - 1'''
 
+query3 = '''SELECT user_id,hosp_id FROM asrim_nwh_users 
+WHERE eff_end_dt is null 
+AND crt_dt >= trunc(sysdate) - 1'''
+cursor.execute(query1)
+df1 = pd.DataFrame(cursor.fetchall(), columns=[col[0] for col in cursor.description])
+print(df1)
+
+cursor.execute(query2)
+df2 = pd.DataFrame(cursor.fetchall(), columns=[col[0] for col in cursor.description])
+print(df2)
+
+cursor.execute(query3)
+df3 = pd.DataFrame(cursor.fetchall(), columns=[col[0] for col in cursor.description])
+print(df3)
 # Close Oracle cursor
 cursor.close()
 
@@ -72,54 +99,25 @@ print("Before creating PostgreSQL connection")
 postgres_conn = create_postgres_connection()
 print("After creating PostgreSQL connection")
 
-# Function to update password in ysr.tmp_user_mst if it's changed
-def update_password_if_changed(conn, login, new_password):
-    try:
-        cursor = conn.cursor()
-        # Fetch the current password from PostgreSQL
-        query = "SELECT pswd FROM ysr.tmp_user_mst WHERE login_name = %s"
-        cursor.execute(query, (login,))
-        current_password = cursor.fetchone()[0]
-
-        # Check if the new password is different from the current password
-        if current_password != new_password:
-            update_query = "UPDATE ysr.tmp_user_mst SET pswd = %s WHERE login_name = %s"
-            cursor.execute(update_query, (new_password, login))
-            conn.commit()
-            print(f"Password for LOGIN ID {login} has been updated.")
-        cursor.close()
-    except psycopg2.Error as e:
-        conn.rollback()
-        print("Error updating password:", e)
-
-# Separate DataFrames for existing and non-existing logins
-existing_login = []
-non_existing_login = []
-
 # Iterate through the DataFrame and check if login_id already exists in the database
-for index, row in df.iterrows():
+for index, row in df1.iterrows():
     login = row['LOGIN_ID']
     user = row['USER_NAME']
+    hosp_id = row['HOSP_ID']
+    hosp_name = row['HOSP_NAME']
+    hosp_dist_id = row['HOSP_DIST_ID']
+    hosp_dist_name = row['HOSP_DIST_NAME']
+    hosp_mandal_id = row['HOSP_MANDAL_ID']
+    hosp_mandal_name = row['HOSP_MANDAL_NAME']
     enc_password = row['ENCRIPTED_PASSWORD']
-
-    cursor = postgres_conn.cursor()
-    query = "SELECT login_name FROM ysr.tmp_user_mst WHERE login_name = %s"
-    cursor.execute(query, (login,))
-    result = cursor.fetchone()
-
-    if result:
-        existing_login.append((login, enc_password))
-    else:
-        non_existing_login.append((login, user, enc_password))
-        print(f"LOGIN ID {login} doesn't exist in the database")
 
 # Function to execute insert queries for non-existing logins
 def execute_postgres_insert_queries(conn, insert_queries):
     try:
         cursor = conn.cursor()
         for login, user, enc_password in insert_queries:
-            sql_insert_login = "INSERT INTO ysr.tmp_user_mst (login_name, user_name, pswd) VALUES (%s, %s, %s)"
-            cursor.execute(sql_insert_login, (login, user, enc_password))
+            sql_insert_login = "INSERT INTO medical.md_user_mst (user_name,login_name,pswd,hospital_name,hosp_id,hosp_dist_id,hosp_dist_name,hosp_mandal_id,hoap_mandal_name) VALUES (%s, %s, %s, %s, %s, %s,%s,%s,%s)"
+            cursor.execute(sql_insert_login, (user,login,enc_password,hosp_name,hosp_id,hosp_dist_id,hosp_dist_name,hosp_mandal_id,hosp_mandal_name))
         conn.commit()
         print("Data inserted successfully")
     except psycopg2.Error as e:
@@ -130,11 +128,7 @@ def execute_postgres_insert_queries(conn, insert_queries):
 
 try:
     # Insert non-existing logins
-    execute_postgres_insert_queries(postgres_conn, non_existing_login)
-
-    # Update existing logins if the password has changed
-    for login, enc_password in existing_login:
-        update_password_if_changed(postgres_conn, login, enc_password)
+    execute_postgres_insert_queries(postgres_conn)
 
     # Set the status to success if all operations were successful
     status = "success"
